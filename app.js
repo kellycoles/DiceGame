1ac811e0b3a89ca5aa7e3a043441b9222a6b8e91
kellycoles <kellycoles1@gmail.com>
2020-04-12 18:44:30 -0500

    visably see active player-working now

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ document.querySelector(".btn-roll").addEventListener("click", () => {
         document.getElementById("current-0").textContent = "0";
         document.getElementById("current-1").textContent = "0";
 
-        document.querySelector(".player-0-panel").classList.remove("active");
-        document.querySelector(".player-1-panel").classList.add("active");
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
     }
 });
