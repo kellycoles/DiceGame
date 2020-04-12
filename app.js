0106d2cfc4986ab18e6a5fc1a2568bd721a35ed4
kellycoles <kellycoles1@gmail.com>
2020-04-12 18:29:50 -0500

    current score is recorder for each player- set to 0 if player rolls a 1

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ let scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 
 document.querySelector(".dice").style.display = "none";
-
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
 document.querySelector(".btn-roll").addEventListener("click", () => {
     //1. Random number
@@ -25,5 +28,15 @@ document.querySelector(".btn-roll").addEventListener("click", () => {
     diceDOM.style.display ="block";
     diceDOM.src = "images/dice-" + dice +".png"
     //3. Update the round score IF the number was not a 1.
-
+    if(dice !== 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    } else {
+        //next player
+        activePlayer === 0 ?  activePlayer = 1 :  activePlayer = 0;
+        roundScore = 0;
+        document.getElementById("current-0").textContent = "0";
+        document.getElementById("current-1").textContent = "0";
+    }
 });
