72703647786be0da3460ac4fab6e4146f85adf4b
kellycoles <kellycoles1@gmail.com>
2020-04-12 18:41:42 -0500

    visably see active player

--- app.js
+++ app.js
@@ -38,5 +38,8 @@ document.querySelector(".btn-roll").addEventListener("click", () => {
         roundScore = 0;
         document.getElementById("current-0").textContent = "0";
         document.getElementById("current-1").textContent = "0";
+
+        document.querySelector(".player-0-panel").classList.remove("active");
+        document.querySelector(".player-1-panel").classList.add("active");
     }
 });
